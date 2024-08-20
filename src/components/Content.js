d05c486b0e4fb441cf3436481be23b938830d6af
Jessie Katz <jess.katz888@gmail.com>
2024-08-20 12:02:50 -0400

    update about

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,8 +4,8 @@ export default function Content() {
     return(
         <div className={styles.content}>
             <p className={styles.font}>Hellooo! I'm Jessie Katz. Studying computer science at Tufts University. </p>
-            <p>Some things I like:</p>
-            <p>bouldering boba my cat momo pixel aesthetics rainy days skog playing piano</p>
+            <p className={styles.font_size_2}>Some things I like:</p>
+            <p className={styles.font}>bouldering boba my cat momo pixel aesthetics rainy days skog playing piano</p>
         </div>
     )
 }
